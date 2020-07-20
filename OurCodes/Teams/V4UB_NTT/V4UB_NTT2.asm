0e9c5e19a6316befe571de0d50cf4ae916a79b5f
alond2003 <alond2003@gmail.com>
2020-07-21 00:39:51 +0300

    deleted BestCode dir & added Anti_traps & added V4UB_NTT team

---
+++ OurCodes/Teams/V4UB_NTT/V4UB_NTT2.asm
@@ -0,0 +1,86 @@
+
+;; Anti Callfar K12 - Best So Far
+;; A4 friendly
+;; plus traps (jumps if about to be run over)
+
+%define interval 0xf1
+%define jumpDist 0x2e00
+%define upper_trap_dist 0x12
+%define bottom_trap_dist 0x34
+
+%define TRAP 0x47EB
+
+push ss
+pop es ;; es = StackSeg
+
+mov si,ax
+add si, @copy_loader
+mov di,0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
+mov cl,(@copy_end-@copy_loader)/2
+rep movsw
+
+sub si,@copy_end-@loop
+mov cl,(@end-@loop)/2
+rep movsw
+push ax
+mov ax,TRAP
+stosw
+stosw
+pop ax
+;; is [bp+di] in the right place ??
+mov word[bp+di],0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
+mov word[bp+di+0x2],cs
+
+mov bx,ax
+mov ax,ss
+push cs
+push ds
+pop es ;; es = Arena 
+pop ss ;; ss = Arena
+
+lea cx,[bx+jumpDist]
+mov cl, 0xA4
+;; shl __,cl -> shl __,0x4 |||| cl = movsb ||| cx = nextjumpPlace
+
+add bx,@loop
+lea sp,[bx+@end]
+sub bx,upper_trap_dist+0x2
+mov dx, interval
+mov word[bx],TRAP
+mov si, 0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
+mov word[bx+si],TRAP
+
+@loop:
+pop di ;; di = ip
+pop bp ;; bp = seg
+shl bp,cl ;; bp = 0x10*seg
+mov [bp+di-0x2],cx ;; bomb 0x10*seg+ip-2
+add sp,dx 
+mov bp,[bx] ;; [bx] = bottom trap
+cmp bp,[bx+si] ;; [bx+si] = upper trap
+jz @loop
+mov di,cx ;; cx = nextjumpPlace
+mov ds,ax ;; ax = StackSeg
+movsw
+jmp cx
+@end:
+
+;;;;;;;;;;;;;;;
+@copy_loader:
+movsw
+movsw
+mov cx,(@copy_end-@copy + @end-@loop)/2
+movsw
+rep movsw
+@copy:
+
+add di,bottom_trap_dist
+movsw
+sub di,0x2+bottom_trap_dist + @end-@loop + upper_trap_dist+0x2
+mov bx,di
+movsw
+lds si,[si]
+lea cx,[bx+jumpDist-0x4]
+
+@copy_end:
+;;anti
