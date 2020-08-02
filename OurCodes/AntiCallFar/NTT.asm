0fbb8a3a877782fbe7f9d4244bc840981d98e8b6
alond2003 <alond2003@gmail.com>
2020-08-02 23:08:21 +0300

    NTT optimization

--- OurCodes/AntiCallFar/NTT.asm
+++ OurCodes/AntiCallFar/NTT.asm
@@ -8,78 +8,86 @@
 %define bottom_trap_dist 0x34
 
 %define TRAP 0x47EB
+%define initialSi 0x2+upper_trap_dist + (@anti_loop_end-@anti_loop) + bottom_trap_dist
+%define trueInterval interval-0x4
 
-
-mov bx,ax
-
+mov bx,TRAP
+xchg ax,bx ;; ax = TRAP, bx = loc
 
 push ss
 pop es ;; es = StackSeg
+lea si,[bx+@reset_loader]
+mov di,initialSi
+mov cl,(@anti_loop_end-@reset_loader)/2
+rep movsw ;; copy code to StackSeg
 
-lea si,[bx+@copy_loader]
-mov di,0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
-mov cl,(@copy_end-@copy_loader)/2
-rep movsw
-
-sub si,@copy_end-@loop
-mov cl,(@end-@loop)/2
-rep movsw
-mov ax,TRAP
 stosw
-stosw
+stosw ;; bomb 2 traps in StackSeg
 
-;; is [bp+di] in the right place ??
-mov word[bp+di],0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
-mov word[bp+di+0x2],cs
+mov byte [bp+di],initialSi ;; for lds si,[si]  
+mov word [bp+di+0x2],cs    ;; (after all the copying)
 
-mov ax,ss
+mov ax,ss ;; ax = StackSeg
 push cs
 push ds
 pop es ;; es = Arena 
 pop ss ;; ss = Arena
 
-lea cx,[bx+jumpDist]
-mov cl, 0x64
-;; shl __,cl -> shl __,0x4 |||| cl = movsb ||| cx = nextjumpPlace
+;; prepare sp and cx
+lea sp,[bx+jumpDist+(@anti_loop_end-@reset_loader)]
+lea cx,[bx+jumpDist] ;; cx = jmpLoc
+mov cl, 0x4
+shl bl,cl
+add cl,bl
+cmp cl,0x14
+jnz @skip
+mov cl,0x4
+@skip:
+;; prepare bx,si and dx
+lea bx,[si-(@anti_loop_end-@anti_loop)-upper_trap_dist-0x2]
+mov si, initialSi
+mov dx, trueInterval
 
-lea sp,[bx+@loop-0x4]
-mov dx, interval-0x4
-add bx, @loop-upper_trap_dist-0x2
-mov si, 0x2+upper_trap_dist + (@end-@loop) + bottom_trap_dist
-mov word [bx],TRAP
-mov word [bx+si],TRAP
+;; jmp to jmpLoc
+mov di,cx
+mov ds,ax
+movsw
+jmp cx
 
-@loop:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+@reset_loader:
+movsw
+movsw
+mov cx,(@anti_loop_end-@reset_start)/2
+movsw
+rep movsw
+@reset_start:
+
+add di,bottom_trap_dist
+movsw ;; bomb bottom trap
+sub di,initialSi+0x2
+mov bx,di ;; prepare bx
+movsw ;; bomb top trap
+lds si,[si] ;; ds = Arena, si = initialSi
+lea cx,[bx+jumpDist-0x4] ;; cx = jmpLoc
+@reset_end:
+
+@anti_loop:
 pop di ;; di = ip
 pop bp ;; bp = seg
 shl bp,cl ;; bp = 0x10*seg
 mov [bp+di-0x2],cx ;; bomb 0x10*seg+ip-2
 add sp,dx 
-mov bp,[bx] ;; [bx] = bottom trap
-cmp bp,[bx+si] ;; [bx+si] = upper trap
-jz @loop
+mov di,[bx] ;; [bx] = bottom trap
+cmp di,[bx+si] ;; [bx+si] = upper trap
+jz @anti_loop
 mov di,cx ;; cx = nextjumpPlace
 mov ds,ax ;; ax = StackSeg
 movsw
 jmp cx
-@end:
+@anti_loop_end:
 
-;;;;;;;;;;;;;;;
-@copy_loader:
-movsw
-movsw
-mov cx,(@copy_end-@copy + @end-@loop)/2
-movsw
-rep movsw
-@copy:
-
-add di,bottom_trap_dist
-movsw
-sub di,0x2+bottom_trap_dist + @end-@loop + upper_trap_dist+0x2
-mov bx,di
-movsw
-lds si,[si]
-lea cx,[bx+jumpDist-0x4]
-
-@copy_end:
-;;anti
