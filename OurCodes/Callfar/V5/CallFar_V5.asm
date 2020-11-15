f6070b9bd22b1e7dc40e6294b0458eeeaa7ab049
UltimateBobo <tomsha9@gmail.com>
2020-11-15 17:35:23 +0200

    updated Call Far V5

--- OurCodes/Callfar/V5/CallFar_V5.asm
+++ OurCodes/Callfar/V5/CallFar_V5.asm
@@ -2,31 +2,35 @@
 %define callAmount 51
 %define gap 0x6
 %define calldist ((callAmount * (gap+0x4)) + 0x4)
-;%define calldist 0x202
 %define addsp jumpDist+calldist
-%define copyloop (@loop_end-@loop)/0x2
-%define readcallfar 0x0111 ;; random location in SS
-%define magicseg 0x1004
+%define copyloop ((@loop_end-@loop)/0x2)
+%define copystart ((@copy_end-@copy)/0x2)
+%define deltaSp_loc (copyloop*0x2 + 0x2) ;; + 0x2 because of rep movsw
+
 
 ;mov ax,calldist
 mov si,ax 				;; si = ax
 add si,@copy 			;; si = @copy
 push ss 				;; push ss value to stack
-pop es					;; es = StackSeg
-xor di,di				;; reset di in case of zoms
-mov cx,(@copy_end-@copy)/2 		;; cx = wordsstart
+pop es					;; es = StackSeg				;; reset di in case of zoms
+mov cl,copystart		;; cx = wordsstart
 ;; +-1
-rep movsw				;; copy code to ES (where SS is)
+rep movsw
+
+mov cl,0xf
+div cx ;; dx
+add dx,0xff6
+				;; copy code to ES (where SS is)
 
 push ss					;; push ss value to stack
 pop ds					;; ds = StackSeg
 
-add ah,0x2
 mov al,0xA1				;; ax = closest location ending in 0xA3
 
-mov bx,readcallfar		;; bx = location of where we read call far address
-mov [bx],ax 			;; [bx] = address of call far
-mov word [bx+0x2],magicseg 	;; [bx+0x2] segment of call far
+mov bx,di		;; bx = location of where we read call far address
+stosw 			;; [bx] = address of call far
+mov word [di],dx 	;; [bx+0x2] segment of call far
+mov word [bx+deltaSp_loc],(jumpDist-calldist)
 
 les di,[bx]				;; di = ax, es = magicseg
 
@@ -45,9 +49,7 @@ xor si,si 				;; si = 0x0000
 push ss					;; push ss value to stack
 pop ds					;; ds = StackSeg
 
-mov ax,magicseg
-push ax
-pop ss ;; ss = Magic
+mov ss,[bx+0x2]
 
 mov sp,[bx]
 add sp,calldist		
@@ -56,7 +58,7 @@ add sp,calldist
 
 mov ax,jumpDist
 
-call far [bx]			;; execute call far
+call far [bx+si]			;; execute call far
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; END OF REGULAR CODE
 
@@ -65,16 +67,13 @@ rep movsw				;; write our code
 @loop:
 sub [bx],ax	;; change call far place (bx -= ax)
 
-mov di,[bx]				;; di = call far address				
+les di,[bx]				;; di = call far address				
 
-sub word sp,jumpDist-calldist
+sub sp,[bx+si]
 ;; - gap because add sp,dx was executed after the call far was overwriten
 ;; + 0x4 because call far is first and last to act
 
 mov cl,copyloop			;; reset counter for next time we execute "rep movsw" (cx = 0x00words)
-
-dec bp
-jz short 0x10
 
 movsw					;; write call far in the next address (address = [bx] = di)
 
@@ -84,13 +83,23 @@ sub di,0x3					;; dx = [bx] + 0x1 (address of call far + 0x1)
 
 xor si,si				;; reset si (si = 0x0000)
 
-call far [bx]			;; execute call far
+dec bp 	
+db 0x75 
+		
+db 0xFF	
+		
+db 0x18
+
+db 0xCC
+
 @loop_end:
+
+
 sub sp,dx
 
-call far [bx]
+call far [bx+si]
 
 @copy_end:
 
 sub sp,dx
-call far [bx]			;; will be copied to the call far address
+call far [bx+si]			;; will be copied to the call far address
