25c4373dfe00ba46681c836cafd628843043da66
alond2003 <alond2003@gmail.com>
2021-01-22 22:56:24 +0200

    added VFive and v4UBGR

--- FINAL CODES 2021/phase1/clean_codes/VFive1.asm
+++ FINAL CODES 2021/phase1/clean_codes/VFive1.asm
@@ -1,10 +1,10 @@
-%define JUMP_DIST 0xF800
-%define CALL_AMOUNT 0x8
-%define GAP 0xD
-%define CALL_DIST (CALL_AMOUNT * (GAP + 0x4) + 0x2)
-%define ADD_SP (JUMP_DIST + CALL_DIST)
+%define JUMP_DIST 0xF900
+%define CALL_AMOUNT 0x55
+%define GAP 0x19
+%define CALL_DIST (CALL_AMOUNT * (GAP + 0x4)-0x6)
 %define MAGIC_SEG 0xFFA
 
+%define SHARE_LOC 0x8101
 
 mov si,ax
 push ss
@@ -15,34 +15,30 @@ pop es
 mov cl,(@copy_end - @copy)/0x2
 rep movsw
 
+mov al,0xA7
+mov [SHARE_LOC],ah
+add ah,(JUMP_DIST/0x100)
+
 push ss
 pop ds
-
-mov al,0xA7
-add ah,(JUMP_DIST/0x100)
 
 inc bh
 mov [bx],ax
 mov word [bx+0x2],MAGIC_SEG
 
 mov word [bx+di],0x3
-mov word [bx + (@loop_end - @loop)/0x2],(GAP + 0x4)
+mov word [bx + (@loop_end - @loop)/0x2],(GAP + 0x8)
 
 push cs
 pop es
 push cs
 pop ss
 
-les di,[bx]
-mov bp,CALL_DIST + 0x60
-lea sp,[di+bp]
-mov cl,(@loop_end - @loop)/0x2
-mov si,cx
 mov dx,JUMP_DIST
 mov ax,0xA5F3
+mov bp, -CALL_DIST-0x60
+lea si,[di-0x4]
 
-stosw
-call far [bx]
 
 @copy:
 @loop:
@@ -54,10 +50,11 @@ movsw
 movsw
 dec word [si]
 jz short (@die+0x1)
-sub di,[bx+si]
+dec di
 @die:
 mov si,cx
 call far [bx]
+db 0x64
 @loop_end:
 
 add sp,[bx+si]
